94dd0e6c703deedb2f77f64e40a34fe20c562adf
DisoorS <sdisoor@gmail.com>
2026-02-24 22:33:07 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Day9/Stock Market Anomaly Detection/GMM.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from sklearn.covariance import log_likelihood
+from sklearn.mixture import GaussianMixture
+
+df = pd.read_csv("pca_data.csv")
+X=df[["PCA1", "PCA2"]]
+gmm = GaussianMixture(n_components=2, random_state=42)
+gmm.fit(X)
+
+cluster = gmm.predict(X)
+df["Cluster"] = cluster
+log_livelihood = gmm.score_samples(X)
+log_likelihood_mean = log_livelihood.mean()
+log_likelihood_std = log_livelihood.std()
+print(log_likelihood_mean, log_likelihood_std)
+threshold = log_likelihood_mean - 2 * log_likelihood_std
+anomalies = log_livelihood < threshold
+
+df["Anomaly"] = anomalies.astype(int)
+
+total_anomalies = df["Anomaly"].sum()
+print(f"Total anomalies detected: {total_anomalies}")
+total_clusters = df["Cluster"].nunique()
+print(f"Total clusters formed: {total_clusters}")
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(8,6))
+
+normal = df[df["Anomaly"] == 0]
+anomaly = df[df["Anomaly"] == 1]
+
+plt.scatter(normal["PCA1"], normal["PCA2"])
+plt.scatter(anomaly["PCA1"], anomaly["PCA2"], marker="x", s=100)
+
+plt.xlabel("PCA1")
+plt.ylabel("PCA2")
+plt.title("Anomaly Detection using GMM")
+plt.show()
--- Day9/Stock Market Anomaly Detection/PCA_feature_reduction.py
+++ Day9/Stock Market Anomaly Detection/PCA_feature_reduction.py
@@ -1,11 +1,20 @@
 import pandas as pd
 from sklearn.decomposition import PCA
-
-df = pd.read_csv("preprocessed_data.csv")
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+df = pd.read_csv("Day9\Stock Market Anomaly Detection\preprocessed_data.csv")
 X = df
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
 pca = PCA(n_components=2, random_state=42)
-X_pca = pca.fit_transform(X)
+X_pca = pca.fit_transform(X_scaled)
 
 pca_df = pd.DataFrame(data=X_pca, columns=["PCA1", "PCA2"])
 pca_df.to_csv("pca_data.csv", index=False)
-print(pca_df.head())
+print(pca.explained_variance_ratio_)
+plt.figure(figsize=(8,6))
+plt.scatter(pca_df["PCA1"], pca_df["PCA2"])
+plt.xlabel("PCA1")
+plt.ylabel("PCA2")
+plt.title("PCA Feature Reduction")
+plt.show()
